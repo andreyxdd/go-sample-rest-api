6454f0502ce5873bdc0247075943fa13dcbf889c
andreyxdd <andreyxdd@yandex.ru>
2023-03-29 18:00:05 -0600

    Implemented validation helper function and update the item request model

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/andreyxdd/go-sample-rest-api
 go 1.13
 
 require (
+	github.com/go-playground/validator/v10 v10.12.0 // indirect
 	github.com/gofiber/fiber/v2 v2.43.0
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
--- models/item_request.go
+++ models/item_request.go
@@ -1,7 +1,27 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type ItemRequest struct {
 	Name     string `json:"name"`
 	Price    int    `json:"price"`
 	Quantity int    `json:"quantity"`
+}
+
+func (itemInput ItemRequest) ValidateStruct() []*ErrorResponse {
+	var errors []*ErrorResponse
+
+	validate := validator.New()
+	validationError := validate.Struct(itemInput)
+
+	if validationError != nil {
+		for _, err := range validationError.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.ErrorMessage = getErrorMessage(err)
+			element.Field = err.Field()
+			errors = append(errors, &element)
+		}
+	}
+
+	return errors
 }
